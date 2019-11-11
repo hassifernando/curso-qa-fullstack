0dc8e7a032ed6204e6347c37b195313580d9db72
Fernando Hassi <fernandohassi@gmail.com>
2019-11-11 18:22:44 -0300

    Grab Talkdesk

--- codecept/pages/talkdesk/talkdesk.js
+++ codecept/pages/talkdesk/talkdesk.js
@@ -19,16 +19,28 @@ module.exports = function () {
 
         async encontrarNomeEmpresaImagem() {
 
-            let nomeEmpresa = new Array();
-            nomeEmpresa.push(await I.grabAttributeFrom('//html/body/section[4]/div/div/div/div[1]/div[2]/div/img', 'alt'));
-            nomeEmpresa.push(await I.grabAttributeFrom('//html/body/section[4]/div/div/div/div[1]/div[3]/div/img', 'alt'));
-            nomeEmpresa.push(await I.grabAttributeFrom('//html/body/section[4]/div/div/div/div[1]/div[4]/div/img', 'alt'));
-            nomeEmpresa.push(await I.grabAttributeFrom('//html/body/section[4]/div/div/div/div[1]/div[5]/div/img', 'alt'));
-            nomeEmpresa.push(await I.grabAttributeFrom('//html/body/section[4]/div/div/div/div[1]/div[6]/div/img', 'alt'));
+            var nomeEmpresa = new Array();
+            let valueDiv = 2;
 
-            console.log(nomeEmpresa[0]);
+            I.amOnPage('https://www.talkdesk.com/');
+            //descer até a parte da página onde está contido as imagens com o nome dos clientes
+            I.waitForVisible('//html/body/section[4]/div/div/div/div[1]/div[1]/h2', 20);
+            I.scrollTo('//html/body/section[4]/div/div/div/div[1]/div[1]/h2');
 
+            while (valueDiv <= 12) {
+                if (valueDiv == 7) {
+                    valueDiv++;
+                };
+                nomeEmpresa.push(await I.grabAttributeFrom(`//html/body/section[4]/div/div/div/div[1]/div[${valueDiv}]/descendant::img`, 'alt'));
+                valueDiv++;
+            };
+            I.amOnPage('https://www.talkdesk.com/customers/');
+            I.waitForVisible('//html/body/section[4]/div/div/div', 20);
+            I.scrollTo('//html/body/section[4]/div/div/div');
+
+            nomeEmpresa.forEach((value, index) => {
+                I.seeElement(`//img[@alt="${nomeEmpresa[index]}"]/ancestor::div[contains(@class, "logo__wrapper col-6 col-sm-4 col-md col-xl -gutter-bottom")]`)
+            })
         }
-
     })
 }
--- codecept/tests/talkdesk/GrabWithImages_test.js
+++ codecept/tests/talkdesk/GrabWithImages_test.js
@@ -2,9 +2,6 @@
 Feature('@HOME - @TALKDESK');
 
 Scenario('test something', async function (I, talkdesk) {
-    I.amOnPage('https://www.talkdesk.com/');
-    I.scrollTo('//html/body/section[4]/div/div/div/div[1]/div[1]/h2');
-    talkdesk.encontrarNomeEmpresaImagem();
-    pause();
-
+    await talkdesk.encontrarNomeEmpresaImagem();
 });
+
