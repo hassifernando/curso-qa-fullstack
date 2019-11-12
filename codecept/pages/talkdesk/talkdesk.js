0540ed04b39e8a4cf13cbb1c0c6a9e3967726763
Fernando Hassi <fernandohassi@gmail.com>
2019-11-11 23:09:22 -0300

    Comentários em funções e testes

--- codecept/pages/talkdesk/talkdesk.js
+++ codecept/pages/talkdesk/talkdesk.js
@@ -3,41 +3,39 @@ let I;
 
 I = require('../../steps_file.js')();
 
-const SEARCH_INPUT = ''
-
-
 
 module.exports = function () {
     return actor({
 
         ///escrever as funções necessárias para essa page
-
-        filtrarConteudo() {
-            I.fillField('Search GitHub', 'nerd');
-            I.pressKey('Enter');
-        },
-
+        /**
+         * Função que realizará o teste completo
+         */
         async encontrarNomeEmpresaImagem() {
 
             var nomeEmpresa = new Array();
+            //o valueDiv inicia com o valor 2 pois a primeira imagem está nessa div
             let valueDiv = 2;
-
+            //acessa homepage da talkdesk
             I.amOnPage('https://www.talkdesk.com/');
-            //descer até a parte da página onde está contido as imagens com o nome dos clientes
+            //Aguardar até 20 segundos para a página carregar e descer até a seção com as imagens dos clientes
             I.waitForVisible('//html/body/section[4]/div/div/div/div[1]/div[1]/h2', 20);
             I.scrollTo('//html/body/section[4]/div/div/div/div[1]/div[1]/h2');
-
+            //as divs vão de 2 a 12 sendo que a div 7 não contém nenhuma imagem de cliente
             while (valueDiv <= 12) {
                 if (valueDiv == 7) {
                     valueDiv++;
                 };
+                //armazenar o valor do atributo alt no array nomeEmpresa, ao fim deverá ser um array com 10 posições
                 nomeEmpresa.push(await I.grabAttributeFrom(`//html/body/section[4]/div/div/div/div[1]/div[${valueDiv}]/descendant::img`, 'alt'));
                 valueDiv++;
             };
+            //Acessar a página de clientes
             I.amOnPage('https://www.talkdesk.com/customers/');
+            //Aguardar até 20 segundos para a página carregar e descer até a seção com as imagens dos clientes
             I.waitForVisible('//html/body/section[4]/div/div/div', 20);
             I.scrollTo('//html/body/section[4]/div/div/div');
-
+            //será executado o comando que irá tentar visualizar os elementos do array nomeEmpresa
             nomeEmpresa.forEach((value, index) => {
                 I.seeElement(`//img[@alt="${nomeEmpresa[index]}"]/ancestor::div[contains(@class, "logo__wrapper col-6 col-sm-4 col-md col-xl -gutter-bottom")]`)
             })
--- codecept/tests/talkdesk/GrabWithImages_test.js
+++ codecept/tests/talkdesk/GrabWithImages_test.js
@@ -1,7 +1,10 @@
 
 Feature('@HOME - @TALKDESK');
-
-Scenario('test something', async function (I, talkdesk) {
+/**
+ * Objetivo do cenário: entrar na homepage, olhar as 10 imagens de clientes e validar
+ * que as imagens estão aparecendo na página do cliente
+ */
+Scenario('Validar clientes da homepage na página de clientes', async function (I, talkdesk) {
     await talkdesk.encontrarNomeEmpresaImagem();
 });
 
